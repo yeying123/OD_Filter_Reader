12570b7d1c63c15ba5bc550d51f95a371bc03e5c
Yeying H <yyj130@gmail.com>
2021-12-29 10:32:33 -0800

    update function to download csv

--- OD_Filter.py
+++ OD_Filter.py
@@ -3,21 +3,29 @@ from collections import namedtuple
 import math
 import pandas as pd
 import streamlit as st
+import base64
 
-st.set_page_config(layout="wide")
-st.sidebar.header('Upload the OD data here')
-st.sidebar.markdown('Make sure the csv file with these columns: "origin", "destination", and "count"')
-st.sidebar.markdown('Note: select one file only')
-
-# Ask to upload file
-uploaded_files = st.sidebar.file_uploader('Choose a CSV file', accept_multiple_files=True, type=['csv'])
-
-df=pd.DataFrame()
-
-delimit=st.sidebar.text_input('Delimiter in csv file:', ';')
+st.set_page_config(
+     page_title="OD Selection from Remix",
+     layout="wide",
+     initial_sidebar_state="expanded")
+st.sidebar.title('OD Selection from Remix')
 
 # Input field to ask for Remix link to extract IDs
-title = st.text_input('Remix Link', 'Copy URL here')
+st.sidebar.header('Step 1: Add Remix Link')
+title = st.sidebar.text_input('Remix Link:', 'Copy URL')
+
+# Ask to upload file
+st.sidebar.header('Step 2: Upload the OD data here')
+st.sidebar.markdown('Make sure the csv file with these columns: "origin", "destination", and "count"')
+st.sidebar.markdown('Note: select one file only')
+uploaded_files = st.sidebar.file_uploader('Choose a CSV file', accept_multiple_files=True, type=['csv'])
+
+# Ask to specify delimiter
+st.sidebar.header('Step 3: Specify delimiter')
+delimit=st.sidebar.text_input('Delimiter in csv file:', ';')
+
+# Read IDs in the link
 if title.find("od=destination")>0:
      from_='destination'
      to_='origin'
@@ -31,24 +39,29 @@ else:
      ID=title[ID_start:]
      st.write('The ID(s): ',ID )
 
+# create empty dataframe (table)
+table=pd.DataFrame()
+
+# Generate the aggregated table
+ID_list=ID.split(",")
 if uploaded_files != []:
      for i in uploaded_files:
           df=pd.read_csv(i,delimiter=delimit)
-          table=df
-          #st.write(table.head(5))
-
-     ID_list=ID.split(",")
-
-     summary=0
-     for t in ID_list:
-          if int(t) in table[from_].values:
-               number=int(t)
-               #st.write(t)
-               df=table.loc[table[from_]==number]
-               total=df['count'].sum()
-               st.write("Total travel from ", from_, " ", number , "is: ", total)
-               summary+=total
-          else:
-               st.write("Total travel from ", from_, " ", number, "is: ", 'No matched record')
-
-     st.write("Sum: ",summary)
+          for t in ID_list:
+               if int(t) in df[from_].values:
+                    number=int(t)
+                    df1=df.loc[df[from_]==number]
+                    table=table.append(df1)
+     
+     def get_table_download_link(df):
+               """Generates a link allowing the data in a given panda dataframe to be downloaded
+               in:  dataframe
+               out: href string
+               """
+               csv = df.to_csv(index=False)
+               b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+               href = f'<a href="data:file/csv;base64,{b64}">Download table as csv file</a>'
+               return href
+     
+     st.dataframe(table,600, 600)
+     st.markdown(get_table_download_link(table), unsafe_allow_html=True)
